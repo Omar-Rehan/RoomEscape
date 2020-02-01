d1038fe1722a41412d06c8d930c002ec18263f8f
Omar Rehan <omar.se.rehan@gmail.com>
2020-02-01 12:00:08 +0200

    initial commit

---
+++ Source/RoomEscape/RoomEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoomEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMESCAPE_API ARoomEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
